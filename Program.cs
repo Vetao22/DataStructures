6c597388885b810b66baa91f6788e61b3b3c0ad6
Everton <zecsharp@hotmail.com>
2021-08-20 14:39:28 -0300

    ClassicArray

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Structures.ClassicArray<int> arr = new Structures.ClassicArray<int>();
+
+            arr.Add(0);
+            arr.Add(1);
+            
+            arr.Insert(5, 2);
+
+            arr.Print();
+        }
+    }
+}
